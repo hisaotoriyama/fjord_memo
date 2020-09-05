7f3680caaada62c832cfb2bf303aca23c35457ac
hisaotoriyama <hisao.toriyama@gmail.com>
2020-09-05 20:27:13 +0900

    change to postgredb base

--- memo.rb
+++ memo.rb
@@ -4,9 +4,10 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'sinatra/content_for'
 require 'json'
+require 'pg'
 
 get '/' do
-  @file_contents = read_file('memo.json')
+  @contents = read_all
   erb :top
 end
 
@@ -15,20 +16,12 @@ get '/new' do
 end
 
 post '/create' do
-  @file_contents = read_file('memo.json')
-  new_id = if @file_contents == []
-             1
-           else
-             @file_contents.last['id'] + 1
-           end
-  @file_contents << { "id": new_id, "todo": params['todo'], "detail": params['detail'] }
-  write_file(@file_contents, 'memo.json')
+  add_new
   redirect '/'
 end
 
 get '/show/:id' do
-  @file_contents = read_file('memo.json')
-  @content =  select_content(@file_contents)
+  @content = read_selected
   erb :show
 end
 
@@ -40,22 +33,12 @@ get '/edit/:id' do
 end
 
 patch '/' do
-  @file_contents = read_file('memo.json')
-  @content = select_content(@file_contents)
-  @content['todo'] = params['todo']
-  @content['detail'] = params['detail']
-  @id = params['id'].to_i
-  @file_contents[@id - 1] = @content
-  write_file(@file_contents, 'memo.json')
+  update_selected
   redirect '/'
 end
 
 delete '/delete/:id' do
-  @file_contents = read_file('memo.json')
-  @contents = @file_contents.reject do |content|
-    content['id'] == params['id'].to_i
-  end
-  write_file(@contents, 'memo.json')
+  delete_selected
   redirect '/'
 end
 
@@ -63,20 +46,46 @@ get '/layout' do
   erb :layout
 end
 
-def read_file(json_file)
-  File.open(json_file) do |file|
-    JSON.load(file)
-  end
+def read_all
+  connection = PG.connect(dbname: 'memodb', port: '5432')
+  results = connection.exec('SELECT * FROM memo_table;')
+  convert_memodata(results)
 end
 
-def write_file(file_contents, file)
-  File.open(file, 'w') do |f|
-    JSON.dump(file_contents, f)
-  end
+def read_selected
+  id_num = params['id'].to_i
+  connection = PG.connect(dbname: 'memodb', port: '5432')
+  results = connection.exec("SELECT * FROM memo_table WHERE id ='#{id_num}';")
+  convert_memodata(results).first
 end
 
-def select_content(file_contents)
-  file_contents.select do |content|
-    content['id'] == params['id'].to_i
-  end.first
+def convert_memodata(results)
+  all_data = []
+  results.each do |result|
+    # #idがなぜかntegerからstringsになっていたので修正
+    result['id'] = result['id'].to_i
+    all_data << result
+  end
+  all_data
+end
+
+def delete_selected
+  id_num = params['id'].to_i
+  connection = PG.connect(dbname: 'memodb', port: '5432')
+  connection.exec("DELETE FROM memo_table WHERE id ='#{id_num}';")
+end
+
+def update_selected
+  id_num = params['id'].to_i
+  todo = params['todo']
+  detail = params['detail']
+  connection = PG.connect(dbname: 'memodb', port: '5432')
+  connection.exec("UPDATE memo_table SET todo = '#{todo}', detail='#{detail}' WHERE id ='#{id_num}';")
+end
+
+def add_new
+  todo = params['todo']
+  detail = params['detail']
+  connection = PG.connect(dbname: 'memodb', port: '5432')
+  connection.exec("INSERT INTO memo_table (todo, detail) VALUES ('#{todo}', '#{detail}');")
 end
